56db74f6e4d4f7b058e475efc098507c2859aa55
Alex Caston (Aex12) <alexcastonmolero@gmail.com>
2026-03-24 02:41:52 +0100

    fix(kubernetes): test and lint errors

--- pipeline/backend/kubernetes/kubernetes_test.go
+++ pipeline/backend/kubernetes/kubernetes_test.go
@@ -47,7 +47,7 @@ func TestGettingConfig(t *testing.T) {
 	config.PodLabels = nil
 	config.PodAnnotations["a2"] = "v2"
 	config.ImagePullSecretNames = append(config.ImagePullSecretNames, "docker.io")
-	config.EnforcedSecurityContext.RunAsNonRoot = newBool(true)
+	config.DefaultSecurityContext.RunAsNonRoot = newBool(true)
 
 	assert.Equal(t, "default", engine.config.Namespace)
 	assert.Equal(t, "hdd", engine.config.StorageClass)
@@ -56,7 +56,7 @@ func TestGettingConfig(t *testing.T) {
 	assert.Len(t, engine.config.PodLabels, 1)
 	assert.Len(t, engine.config.PodAnnotations, 1)
 	assert.Len(t, engine.config.ImagePullSecretNames, 1)
-	assert.False(t, *engine.config.EnforcedSecurityContext.RunAsNonRoot)
+	assert.False(t, *engine.config.DefaultSecurityContext.RunAsNonRoot)
 }
 
 func TestSetupWorkflow(t *testing.T) {
